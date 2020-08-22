9ed251d270bc9ebe85eaf905afdba6324b986b2d
IsmaelV <iv2181@columbia.edu>
2020-08-21 18:29:37 -0700

    Updated argument parser to more pythonic way

--- HW1/full-name-predictor.py
+++ HW1/full-name-predictor.py
@@ -4,10 +4,23 @@ Given two or more names with first and surnames, identify the full name of the f
 :author: Ismael Villegas-Molina
 """
 import pandas as pd
-import sys
+import argparse
+
+
+def read_data(path_to_test_data):
+	return pd.read_csv("./data/dev-key.csv", header=None), pd.read_csv(path_to_test_data, header=None)
+
+
+def check_arguments():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("path_to_test_data", metavar="data-path", help="Path to the csv file with test data", type=str)
+	return parser.parse_args()
+
 
 if __name__ == "__main__":
-	train = pd.read_csv("./data/dev-key.csv", header=None)
+	arguments = check_arguments()
+
+	train, test = read_data(arguments.path_to_test_data)
+
 	print(train.head(5))
-	test = pd.read_csv(sys.argv[1], header=None)
 	print(test.head(5))
