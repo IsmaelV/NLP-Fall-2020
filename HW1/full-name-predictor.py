2c67e101da1cef1db7273822943492a8eb0ec58a
IsmaelV <iv2181@columbia.edu>
2020-08-21 18:12:37 -0700

    Retrieve test set from command line argument

--- HW1/full-name-predictor.py
+++ HW1/full-name-predictor.py
@@ -1,10 +1,13 @@
 """
-Full Name Predictor:
+Full Name Predictor
 Given two or more names with first and surnames, identify the full name of the first person.
 :author: Ismael Villegas-Molina
 """
 import pandas as pd
+import sys
 
 if __name__ == "__main__":
 	train = pd.read_csv("./data/dev-key.csv", header=None)
 	print(train.head(5))
+	test = pd.read_csv(sys.argv[1], header=None)
+	print(test.head(5))
