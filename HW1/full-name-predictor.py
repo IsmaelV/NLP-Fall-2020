cdaedf35b4fa91e2e29a7e42a7659fbf8ded22e6
IsmaelV <iv2181@columbia.edu>
2020-08-25 12:46:54 -0700

    Read surnames and male/female forenames | Added logic for predictions | Currently at 0.877 accuracy

--- HW1/full-name-predictor.py
+++ HW1/full-name-predictor.py
@@ -4,7 +4,6 @@ Given two or more names with first and surnames, identify the full name of the f
 :author: Ismael Villegas-Molina
 """
 import argparse
-import csv
 
 
 def read_data():
@@ -26,7 +25,6 @@ def check_arguments():
 
 
 def surnames():
-	rank = dict()
 	frequency = dict()
 	count = 0
 	with open("./data/Surnames_2010Census.csv") as f:
@@ -34,11 +32,28 @@ def surnames():
 		for line in f:
 			csv_row = line.strip().split(',')
 			count += int(csv_row[2])
-			rank[csv_row[0]] = int(csv_row[1])
 			frequency[csv_row[0]] = int(csv_row[2])
 	for key in frequency.keys():
 		frequency[key] = frequency[key] / count
-	return rank, frequency
+	return frequency
+
+
+def female_male_names():
+	female_frequency = dict()
+	with open("./data/female_names.txt") as f:
+		next(f)
+		for line in f:
+			row = line.strip().split('\t')
+			female_frequency[row[0]] = float(row[1])
+
+	male_frequency = dict()
+	with open("./data/male_names.txt") as f:
+		next(f)
+		for line in f:
+			row = line.strip().split('\t')
+			male_frequency[row[0]] = float(row[1])
+
+	return female_frequency, male_frequency
 
 
 def rule_1(first_tokens, second_tokens):
@@ -53,13 +68,73 @@ def rule_2(first_tokens, second_tokens):
 	return False, ""
 
 
-def rule_3(first_tokens, second_tokens):
-	print()
-	return False, ""
+def add_name(all_surnames, female, male, forename=False):
+	first_over_surname = []
+	for s in range(len(all_surnames)):
+		if all_surnames[s] > 0 and all_surnames[s] > female[s] and all_surnames[s] > male[s]:
+			if forename:
+				first_over_surname.append(False)
+			else:
+				first_over_surname.append(True)
+			continue
+		if forename:
+			first_over_surname.append(True)
+		else:
+			first_over_surname.append(False)
+	return first_over_surname
+
+
+def create_new_name(f_tokens, l_tokens, f_surnames, l_surnames, f_female, f_male, l_female, l_male):
+	new_name = f_tokens[0]
+	add_forename = add_name(f_surnames, f_female, f_male, forename=True)
+	for j in range(1, len(add_forename)):
+		if add_forename[j]:
+			new_name += " " + f_tokens[j]
+
+	add_surname = add_name(f_surnames, f_female, f_male)
+	if True in add_surname:
+		for j in range(1, len(add_surname)):
+			if add_surname[j]:
+				new_name += " " + f_tokens[j]
+	else:
+		add_surname = add_name(l_surnames, l_female, l_male)
+		for j in range(1, len(add_surname)):
+			if add_surname[j]:
+				new_name += " " + l_tokens[j]
+
+	return new_name
 
 
 if __name__ == "__main__":
 	arguments = check_arguments()
 
 	former, latter, keys = read_data()
-	surname_rank, surname_freq = surnames()
+	surname_freq = surnames()
+	female_freq, male_freq = female_male_names()
+
+	correct = 0
+	result = open("predictions.txt", "w")
+	for i in range(len(keys)):
+		former_tokens = former[i].split()
+		latter_tokens = latter[i].split()
+		former_surname_mask = [-1]
+		former_female_mask = [1]
+		former_male_mask = [1]
+		for fs in former_tokens[1:]:
+			former_surname_mask.append(surname_freq.get(fs, -1))
+			former_female_mask.append(female_freq.get(fs, -1))
+			former_male_mask.append(male_freq.get(fs, -1))
+		latter_surname_mask = [-1]
+		latter_female_mask = [1]
+		latter_male_mask = [1]
+		for ls in latter_tokens[1:]:
+			latter_surname_mask.append(surname_freq.get(ls, -1))
+			latter_female_mask.append(female_freq.get(ls, -1))
+			latter_male_mask.append(male_freq.get(ls, -1))
+		my_prediction = create_new_name(former_tokens, latter_tokens, former_surname_mask, latter_surname_mask, former_female_mask, former_male_mask, latter_female_mask, latter_male_mask)
+		result.write(my_prediction + "\n")
+		if my_prediction == keys[i]:
+			correct += 1
+
+	result.close()
+	print("Percent correct:", correct/len(keys))
