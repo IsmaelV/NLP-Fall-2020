9cc2d2a5c3babc05d9100d014d9958e0c2332b9e
IsmaelV <iv2181@columbia.edu>
2020-08-26 20:49:10 -0700

    Added single_name_predicted rule | If we are to add a surname from the latter and no latter surname was given, simply append the last latter token to the prediction | Accuracy at 0.933

--- HW1/full-name-predictor.py
+++ HW1/full-name-predictor.py
@@ -6,16 +6,21 @@ Given two or more names with first and surnames, identify the full name of the f
 import argparse
 
 
-def read_train_key():
+def read_train():
 	"""
 	Reads the train data and grabs the ground truth
 	:return: List of target full name for former name
 	"""
 	key_names = []
+	former = []
+	latter = []
 	for line in open("./data/dev-key.csv"):
 		csv_row = line.strip().split(',')
+		former_name, latter_name = csv_row[0].split(" AND ")
+		former.append(former_name)
+		latter.append(latter_name)
 		key_names.append(csv_row[1])
-	return key_names
+	return key_names, former, latter
 
 
 def read_test(path_to_test):
@@ -23,13 +28,13 @@ def read_test(path_to_test):
 	Reads the test data
 	:return: List of former names, List of latter names
 	"""
-	first_names = []
-	second_names = []
+	former = []
+	latter = []
 	for line in open(path_to_test):
 		row = line.strip().split(" AND ")
-		first_names.append(row[0])
-		second_names.append(row[1])
-	return first_names, second_names
+		former.append(row[0])
+		latter.append(row[1])
+	return former, latter
 
 
 def check_arguments():
@@ -69,14 +74,14 @@ def female_male_names():
 	with open("./data/female_names.txt") as f:
 		next(f)
 		for line in f:
-			row = line.strip().split('\t')
+			row = line.strip().split()
 			female_frequency[row[0]] = float(row[1])
 
 	male_frequency = dict()
 	with open("./data/male_names.txt") as f:
 		next(f)
 		for line in f:
-			row = line.strip().split('\t')
+			row = line.strip().split()
 			male_frequency[row[0]] = float(row[1])
 
 	return female_frequency, male_frequency
@@ -92,6 +97,40 @@ def rule_2(first_tokens, second_tokens):
 	if len(first_tokens) == 1 and len(second_tokens) == 3:
 		return True, first_tokens[0] + " " + second_tokens[2]
 	return False, ""
+
+
+def single_name_predicted(init_prediction, l_tokens, l_female, l_male, l_surname):
+	"""
+	Given that the prediction was a single name (not possible), we perform operations to add a surname. The initial way
+	is to simply add the last token of the latter name to the prediction. The second way (commented) is to test the
+	name frequencies of female and male names vs. surnames.
+	:param init_prediction: String of initial prediction with no surname
+	:param l_tokens: List of string tokens of latter name
+	:param l_female: List of integers of frequencies of female name probability of latter name
+	:param l_male: List of integers of frequencies of male name probability of latter name
+	:param l_surname: List of integers of frequencies of surname probability of latter name
+	:return: Updated prediction with a surname
+	"""
+	# boolean_use = []
+	# for i in range(1, len(l_tokens)):
+	# 	if l_female[i] == -1 or l_male[i] == -1:
+	# 		boolean_use.append(True)
+	# 	elif l_female[i] < l_male[i]:
+	# 		if l_male[i] - l_surname[i] < 0.01:
+	# 			boolean_use.append(True)
+	# 	elif l_female[i] >= l_male[i]:
+	# 		if l_female[i] - l_surname[i] < 0.01:
+	# 			boolean_use.append(True)
+	# 	else:
+	# 		boolean_use.append(False)
+	#
+	# for k in range(len(boolean_use)):
+	# 	if boolean_use[k]:
+	# 		init_prediction += " " + l_tokens[k+1]
+	#
+	# return init_prediction
+
+	return init_prediction + " " + l_tokens[-1]
 
 
 def add_name(all_surnames, female, male, forename=False):
@@ -147,10 +186,14 @@ def create_new_name(f_tokens, l_tokens, f_surnames, l_surnames, f_female, f_male
 			if add_surname[j]:
 				new_name += " " + f_tokens[j]
 	else:  # If surname does not exist in former name, check the surnames in the latter name
+		added_surname = False
 		add_surname = add_name(l_surnames, l_female, l_male)
 		for j in range(1, len(add_surname)):
 			if add_surname[j]:
 				new_name += " " + l_tokens[j]
+				added_surname = True
+		if not added_surname:  # If no surname was added, then simply append the last latter token to prediction
+			new_name += " " + l_tokens[-1]
 
 	return new_name
 
@@ -158,9 +201,9 @@ def create_new_name(f_tokens, l_tokens, f_surnames, l_surnames, f_female, f_male
 def evaluate_predictions():
 	"""
 	Read the predictions from predictions.txt made from create_predictions()
-	:return: Precision of the predictions
+	:return: Accuracy of the predictions
 	"""
-	true_answers = read_train_key()
+	true_answers, former, latter = read_train()  # Former and Latter are extracted for debugging
 	predictions = open("predictions.txt", "r")
 	ind = 0
 	correct = 0
@@ -168,9 +211,13 @@ def evaluate_predictions():
 		prediction = p.strip()
 		if prediction == true_answers[ind]:
 			correct += 1
+		else:
+			print("================================")
+			print("Prediction:\t\t", prediction)
+			print("Correct Ans:\t", true_answers[ind])
 		ind += 1
 	predictions.close()
-	print("Prediction precision:", correct/len(true_answers))
+	print("Prediction accuracy:", correct/len(true_answers))
 	return correct/len(true_answers)
 
 
@@ -219,6 +266,10 @@ def create_predictions(path_to_test, file_to_write):
 		# --------------------------------
 		my_prediction = create_new_name(former_tokens, latter_tokens, former_surname_mask, latter_surname_mask,
 										former_female_mask, former_male_mask, latter_female_mask, latter_male_mask)
+
+		if len(my_prediction.strip().split()) == 1:
+			my_prediction = single_name_predicted(my_prediction, latter_tokens, latter_female_mask, latter_male_mask,
+													latter_surname_mask)
 
 		# --------------------------------
 		# Write results into file
