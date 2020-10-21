630eb63138a5d2e72f32deed4f3c7c744cf3292a
IsmaelV <iv2181@columbia.edu>
2020-10-20 19:34:56 -0700

    Created state transition probabilities from data | Check to see if we use tags or words, currently using tags

--- HW5/hmmlearn.py
+++ HW5/hmmlearn.py
@@ -20,10 +20,42 @@ class HMM(object):
 		self.all_training_data = None
 		self.states = set()
 		self.observations = set()
+		self.state_transition_prob = {"START": dict()}
 
 	def load_new(self, all_training_data):
-		self.all_training_data = all_training_data
+		self.read_data(all_training_data)
+
+	def read_data(self, d):
+		f = open(d, encoding="utf8")
+
+		# Read each sentence and store counts
+		for sent in f:
+			prev_state = "START"
+			for w in sent.split():
+				word, tag = w.rsplit("/", 1)
+
+				self.states.add(tag)
+				self.observations.add(word)
+
+				# TODO: Need to do emission probabilities, and see if it's words or tags
+				# State transitions will be for tags, NOT WORDS
+				if prev_state not in self.state_transition_prob:
+					self.state_transition_prob[prev_state] = dict()
+				tmp_dict = self.state_transition_prob[prev_state]
+				tmp_val = tmp_dict.get(tag, 0) + 1
+				tmp_dict[tag] = tmp_val
+				# self.state_transition_prob[prev_state] = tmp_dict  # This might be unnecessary
+				prev_state = tag
+
+		# Convert counts into probabilities
+		for k in self.state_transition_prob.keys():
+			my_dict = self.state_transition_prob[k]
+			tot_val = sum(my_dict.values())
+			for j in my_dict.keys():
+				my_dict[j] /= tot_val
 
 
 if __name__ == "__main__":
 	arguments = check_arguments()
+	my_hmm = HMM()
+	my_hmm.load_new(arguments.path_to_input)
